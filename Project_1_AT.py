15826cf3d0e41a110dc145e2b05db713cdc898ba
Arjunt10 <atripathi@ryerson.ca>
2024-10-10 10:53:20 -0400

    Create Project_1_AT.py
    Finished Step 1: Data Processing
    
    Working on Step 2: Data Visualization.

---
+++ Project_1_AT.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  9 20:31:48 2024
+
+@author: arjun
+
+Arjun Tripathi
+501 021 964
+AER850 - PROJECT 1
+"""
+
+"STEP 1 - Data Processing"
+
+"Library Import"
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+"Read Data from CSV File & convert into dataframe"
+
+df = pd.read_csv("Project_1_Data.csv")
+
+"STEP 2: Data Visualization "
+
