cc8487b831de823146549d1bfff4f18139d706f9
Arjunt10 <atripathi@ryerson.ca>
2024-10-10 20:57:45 -0400

    Added Scatterplot
    working on other plots.

--- Project_1_AT.py
+++ Project_1_AT.py
@@ -22,3 +22,43 @@ df = pd.read_csv("Project_1_Data.csv")
 
 "STEP 2: Data Visualization "
 
+"Need to visualize the data in plots"
+
+"Histogram"
+# Histogram = plt.hist(df['X'], bins= 45)
+
+
+
+"Scatter Plot"
+
+scatterplot_X = plt.scatter(df['Step'], df['X'])
+
+scatterplot_X = plt.scatter(df['Step'], df['Y'])
+
+scatterplot_X = plt.scatter(df['Step'], df['Z'])
+
+plt.title('Scatterplot of X, Y, Z vs Steps')
+plt.xlabel('Steps')
+plt.ylabel('X, Y, Z')
+
+
+
+
+""
+
+"Simple Statistics from the data"
+
+stats = df.describe()
+
+print ("The statistically measures of the columns in the dataset are:", stats)
+
+
+
+
+
+"STEP 3: Correlation Analysis"
+
+correlation_matrix = df.corr()
+
+print ("Here is the correlation matrix of the dataset", correlation_matrix)
+
