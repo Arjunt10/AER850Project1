d7d7874f8b5a296e797b0d34ee9d67d408e48f04
Arjunt10 <atripathi@ryerson.ca>
2024-10-20 12:27:03 -0400

    Working on Model 2: Random Forest
    - Commented out plots for now.
    
    - Ran this code for model 2 and got a bunch of errors.
    
    SOlution: cannot use regressor for classification problem, need to use randomforestclassifer.

--- Project_1_AT.py
+++ Project_1_AT.py
@@ -24,51 +24,51 @@ initial_data = df.head()
 
 print ("Here are the first few rows of the data for verification:", initial_data)
 
-"STEP 2: Data Visualization Of RAW DATA "
+# "STEP 2: Data Visualization Of RAW DATA "
 
-# "Need to visualize the raw data in plots"
+# # "Need to visualize the raw data in plots"
 
-"3D Scatter Plot of RAW DATA"
+# "3D Scatter Plot of RAW DATA"
 
-fig = plt.figure (figsize=(10,10))
-ax = fig.add_subplot(111,projection = '3d')
+# fig = plt.figure (figsize=(10,10))
+# ax = fig.add_subplot(111,projection = '3d')
 
-scatterplot_raw_XYZ = ax.scatter(df['X'],df['Y'],df['Z'], c=df['Step'],cmap = 'viridis')
+# scatterplot_raw_XYZ = ax.scatter(df['X'],df['Y'],df['Z'], c=df['Step'],cmap = 'viridis')
 
-plt.title('Scatterplot of X, Y, Z (RAW DATA)')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
+# plt.title('Scatterplot of X, Y, Z (RAW DATA)')
+# ax.set_xlabel('X')
+# ax.set_ylabel('Y')
+# ax.set_zlabel('Z')
 
-# For the legend
-plt.colorbar(scatterplot_raw_XYZ,ax=ax, label = 'step')
+# # For the legend
+# plt.colorbar(scatterplot_raw_XYZ,ax=ax, label = 'step')
 
-"Box Plot of RAW DATA"
-# To see if any outliers exist for each axis
+# "Box Plot of RAW DATA"
+# # To see if any outliers exist for each axis
 
-fig_2 = plt.figure(figsize=(10,6))
-plt.boxplot([df['X'], df['Y'], df['Z']], labels = ['X', 'Y', 'Z'])
-plt.title('Box Plot of X, Y, Z (RAW DATA)')
-
-
-"Simple Statistics from the Raw data"
-
-stats = df.describe()
-
-print ("The statistically measures of the columns in the raw dataset are:", stats)
+# fig_2 = plt.figure(figsize=(10,6))
+# plt.boxplot([df['X'], df['Y'], df['Z']], labels = ['X', 'Y', 'Z'])
+# plt.title('Box Plot of X, Y, Z (RAW DATA)')
 
 
-"STEP 3: Correlation Analysis of RAW Data"
+# "Simple Statistics from the Raw data"
 
-correlation_matrix = df.corr()
+# stats = df.describe()
 
-print ("Here is the correlation matrix of the raw dataset", correlation_matrix)
+# print ("The statistically measures of the columns in the raw dataset are:", stats)
 
-# Need to create a new figure
 
-plt.figure(figsize=(10,10))
+# "STEP 3: Correlation Analysis of RAW Data"
 
-correlation_heatmap = sns.heatmap(correlation_matrix)
+# correlation_matrix = df.corr()
+
+# print ("Here is the correlation matrix of the raw dataset", correlation_matrix)
+
+# # Need to create a new figure
+
+# plt.figure(figsize=(10,10))
+
+# correlation_heatmap = sns.heatmap(correlation_matrix)
 
 "Step 4: Classification Model Development/Engineering"
 
@@ -131,4 +131,31 @@ best_logreg_model1 = m1_logreg_grid.best_estimator_
 print ('Best Logistic Regression Model:', best_logreg_model1)
 
 "MODEL 2: Random Forest Model (Using GridSearchCV)"
+
+#Import library for Model 2
+from sklearn.ensemble import RandomForestRegressor
+
+m2_randomforest = RandomForestRegressor(random_state = 42)
+
+# Defining Parameters
+m2_randomforest_param_grid = {
+    'n_estimators': [10, 30, 50],
+    'max_depth': [None, 10, 20, 30],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4],
+    'max_features': ['sqrt', 'log2']
+    }
+
+m2_randomforest_grid = GridSearchCV((m2_randomforest),
+                                    m2_randomforest_param_grid,
+                                    scoring = 'accuracy',
+                                    n_jobs = -1,
+                                    cv = 5)
+#Fitting model with scaled features and trainset target
+m2_randomforest_grid.fit(features_train_scaled, target_train)
+
+#Gives out best parameters for random forest model
+
+best_randomforest_model2 = m2_randomforest_grid.best_estimator_
+print ('Best Random Forest Model:', best_randomforest_model2)
 
